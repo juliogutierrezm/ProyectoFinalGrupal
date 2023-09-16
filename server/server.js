73be77d91fff830fcd5863867f1ef1452fdc982c
juliogutierrezm <juliogutierreztrader@gmail.com>
2023-09-15 18:12:42 -0600

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const cors = require("cors");
+const express = require("express");
+require("../server/config/mongoose.config");
+const app = express();
+require("../server/config/mongoose.config"); 
+const userRoutes = require('./routes/userRoutes'); 
+require('dotenv').config();
+
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.json()); // This is new
+app.use(express.urlencoded({ extended: true })); 
+app.use('/', userRoutes);
+ 
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server started at port 8080")
+});
